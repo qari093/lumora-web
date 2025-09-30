55a24be98ece077ec6f4be2da7cb17fc44d8c3d7
waqarahmad <rana.waqar78@gmail.com>
2025-09-30 20:23:21 +0200

    Fix build: move Pulse config to server layout; clean client pages; ensure "use client" first

--- app/pulse/login/page.tsx
+++ app/pulse/login/page.tsx
@@ -1,9 +1,4 @@
-"use client";
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-export const fetchCache = "force-no-store";
-
-import React from "react";
+"use client";import React from "react";
 import { supaPublic } from "../../../lib/supabase";
 
 export default function Login(){
